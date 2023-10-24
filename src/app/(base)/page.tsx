24b744217cf59eeae35906a57f28d66240e5cf7c
arbaevsherbolot <arbaevsherbolot1@gmail.com>
2023-10-24 23:40:03 +0600

    Update

--- src/app/(base)/page.tsx
+++ src/app/(base)/page.tsx
@@ -1,6 +1,8 @@
 import { useUserData } from "@/hooks/useUserData";
 import HomeClient from "./page.uc";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const data = await useUserData();
 
